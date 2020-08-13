4558d6d31011a0d56a37193e32b846841e3b3e68
ibfahd <ibfahd@gmail.com>
2020-08-13 22:35:40 +0100

    init 01

--- Mark and Toys.py
+++ Mark and Toys.py
@@ -1,29 +1,38 @@
 #!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-import time
-#import operator
-def Prime(n):
-    n = abs(n)
-    if n == 1 :
-        return "Not prime"
-    elif (n == 2) or (n == 3) or (n == 5) or (n == 7):
-        return "Prime"
-    elif n % 2 == 0:
-        return "Not prime"
-    else:
-        for i in range(3, int((math.sqrt(n)))+1, 2):
-            if n % i == 0:
-                return "Not prime"
-    return "Prime"
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    toys = []
+    toysprice = 0
+    for p in prices:
+        ntoys = len(toys)
+        if p <= k:
+            toysprice = toysprice + p
+            if ntoys == 0:
+                toys.append(p)
+            else:
+                for i in range(ntoys-1,0,-1):
+                    if p <= toys[i] and p >= toys[i-1]:
+                        toys.insert(i, p)
+                        break
+                    else:
+                        toys.insert(0, p)
+            if toysprice > k:
+                toysprice = toysprice - toys[ntoys]
+                toys.pop(ntoys)
+    return len(toys)
 
 if __name__ == '__main__':
-    #r = operator.xor(17, 7)
-    #print(r)
-    a = 17
-    b = 7
-    print(a^b)
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nk = input().split()
+    n = int(nk[0])
+    k = int(nk[1])
+    prices = list(map(int, input().rstrip().split()))
+    result = maximumToys(prices, k)
+    fptr.write(str(result) + '\n')
+    fptr.close()
