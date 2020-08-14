83494549c6520269c11e9d967bf7f688f4b49496
ibfahd <ibfahd@gmail.com>
2020-08-14 10:13:43 +0100

    Done

--- Mark and Toys.py
+++ Mark and Toys.py
@@ -10,29 +10,37 @@ def maximumToys(prices, k):
     toys = []
     toysprice = 0
     for p in prices:
-        ntoys = len(toys)
         if p <= k:
             toysprice = toysprice + p
-            if ntoys == 0:
+            if len(toys) == 0:
                 toys.append(p)
             else:
-                for i in range(ntoys-1,0,-1):
-                    if p <= toys[i] and p >= toys[i-1]:
-                        toys.insert(i, p)
-                        break
-                    else:
+                if len(toys) == 1:
+                    if toys[0] > p:
                         toys.insert(0, p)
+                    else:
+                        toys.append(p)
+                else:
+                    if p >= toys[len(toys)-1]:
+                        toys.append(p)
+                    else:
+                        for i in range(len(toys)-1,0,-1):
+                            if p < toys[i] and p >= toys[i-1]:
+                                toys.insert(i, p)
+                                break
+                            elif i == 1:
+                                toys.insert(0, p)
+                                break
             if toysprice > k:
-                toysprice = toysprice - toys[ntoys]
-                toys.pop(ntoys)
+                toysprice = toysprice - toys[len(toys)-1]
+                toys.pop(len(toys)-1)
+    print(toys)
     return len(toys)
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     nk = input().split()
     n = int(nk[0])
     k = int(nk[1])
     prices = list(map(int, input().rstrip().split()))
     result = maximumToys(prices, k)
-    fptr.write(str(result) + '\n')
-    fptr.close()
+    print(result)
